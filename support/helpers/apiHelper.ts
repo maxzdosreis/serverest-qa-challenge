b52552d9c45a0941c9e8ed9997162bf50c9eecb9
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2026-03-17 17:13:59 -0300

    feat: adiciona api helper para autenticação

---
+++ support/helpers/apiHelper.ts
@@ -0,0 +1,22 @@
+import { APIRequestContext, request } from '@playwright/test';
+import { makeUser } from '../factories/userFactory';
+
+export async function getAuthToken(): Promise<string> {
+    const context = await request.newContext({
+        baseURL: process.env.BASE_URL
+    });
+
+    // Cria um usuário administrador
+    const user = makeUser({ administrador: 'true' });
+    await context.post('/usuario', { data: user });
+
+    // Faz login e retorna o token
+    const response = await context.post('/login', {
+        data: { email: user.email, password: user.password},
+    });
+
+    const body = await response.json();
+    await context.dispose();
+
+    return body.authorization;
+}
