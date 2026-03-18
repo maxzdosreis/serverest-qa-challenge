8c172f1e328dd4cf261d5e2a00726a669cc1c582
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2026-03-18 17:25:55 -0300

    feat: adiciona page objects HomePageAdmin

---
+++ pages/HomePageAdmin.ts
@@ -0,0 +1,35 @@
+import { Page, Locator } from '@playwright/test';
+
+export class HomePageAdmin {
+    readonly page: Page;
+    readonly homeLink: Locator;
+    readonly cadastrarUsuariosLink: Locator;
+    readonly listarUsuariosLink: Locator;
+    readonly cadastrarProdutosLink: Locator;
+    readonly listarProdutosLink: Locator;
+    readonly relatoriosLink: Locator;
+    readonly cadastrarUsuariosCard: Locator;
+    readonly listarUsuariosCard: Locator;
+    readonly cadastrarProdutosCard: Locator;
+    readonly listarProdutosCard: Locator;
+    readonly relatoriosCard: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.homeLink = page.getByTestId('home');
+        this.cadastrarUsuariosLink = page.getByTestId('cadastrar-usuarios');
+        this.listarUsuariosLink = page.getByTestId('listar-usuarios');
+        this.cadastrarProdutosLink = page.getByTestId('cadastrar-produtos');
+        this.listarProdutosLink = page.getByTestId('listar-produtos');
+        this.relatoriosLink = page.getByTestId('link-relatorios');
+        this.cadastrarUsuariosCard = page.getByTestId('cadastrarUsuarios');
+        this.listarUsuariosCard = page.getByTestId('listarUsuarios');
+        this.cadastrarProdutosCard = page.getByTestId('cadastrarProdutos');
+        this.listarProdutosCard = page.getByTestId('listarProdutos');
+        this.relatoriosCard = page.getByTestId('relatorios');
+    }
+
+    async navigate() {
+        await this.page.goto('/admin/home')
+    }
+}
